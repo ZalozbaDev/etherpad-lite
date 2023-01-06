259c4c25be6fd7533f7a96959b6fddb85a7902fc
Daniel Zoba <daniel.sobe@sorben.com>
2023-01-06 16:11:26 +0100

    use a versioned tag

--- build_customized_docker_container.sh
+++ build_customized_docker_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --build-arg ETHERPAD_PLUGINS="ep_whiteboard " --tag etherpad_custom .
+docker build --build-arg ETHERPAD_PLUGINS="ep_whiteboard " --tag etherpad-custom-1.8.18 .
 
